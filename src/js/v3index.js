345d212c77c5dc18b0e4905fb08806e213e2733d
Robert Vollmert <rob@vllmrt.net>
2019-03-21 22:34:23 +0100

    add doubleback

--- src/js/v3index.js
+++ src/js/v3index.js
@@ -440,7 +440,7 @@ var pstate = {
 			'wagiri','shugaku','hakoiri','roma','toichika','cbblock','nondango','onsen'],
 	orig  :['mochinyoro','ayeheya','aho'],
 	genre :['tapa','arukone','yinyang','building','kropki','starbattle','easyasabc','walllogic'],
-	add   :['angleloop', 'heyawacky', 'nurimisaki']
+	add   :['angleloop', 'doubleback', 'heyawacky', 'nurimisaki']
 };
 var tabstate = {
 	lunch:'lunch', lunch2:'lunch', lunch3:'nigun',
