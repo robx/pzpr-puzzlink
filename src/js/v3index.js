bdae5aad8d6e22dbcc683c0f8b1799122077c944
Robert Vollmert <rob@vllmrt.net>
2019-04-17 16:17:17 +0200

    add scrin

--- src/js/v3index.js
+++ src/js/v3index.js
@@ -440,7 +440,7 @@ var pstate = {
 			'wagiri','shugaku','hakoiri','roma','toichika','cbblock','nondango','onsen'],
 	orig  :['mochinyoro','ayeheya','aho'],
 	genre :['tapa','arukone','yinyang','building','kropki','starbattle','easyasabc','walllogic'],
-	add   :['angleloop', 'doubleback', 'heyawacky', 'nurimisaki', 'meander', 'satogaeri']
+	add   :['angleloop', 'doubleback', 'heyawacky', 'nurimisaki', 'meander', 'satogaeri', 'scrin']
 };
 var tabstate = {
 	lunch:'lunch', lunch2:'lunch', lunch3:'nigun',
