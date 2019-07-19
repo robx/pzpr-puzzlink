a638cf4ec4ae113cb8ff2285358eabd7ed446e3f
Robert Vollmert <rob@vllmrt.net>
2019-07-19 19:26:17 +0200

    add geradeweg

--- src/js/v3index.js
+++ src/js/v3index.js
@@ -441,7 +441,8 @@ var pstate = {
 			'wagiri','shugaku','hakoiri','roma','toichika','cbblock','nondango','onsen'],
 	orig  :['mochinyoro','ayeheya','aho'],
 	genre :['tapa','arukone','yinyang','building','kropki','starbattle','easyasabc','walllogic'],
-	add   :['angleloop', 'doubleback', 'heyawacky', 'nurimisaki', 'meander', 'satogaeri', 'scrin', 'heteromino', 'yajilin-regions', 'dbchoco']
+	add   :['angleloop', 'doubleback', 'heyawacky', 'nurimisaki', 'meander', 'satogaeri', 'scrin', 'heteromino',
+			'yajilin-regions', 'dbchoco', 'geradeweg']
 };
 var tabstate = {
 	lunch:'lunch', lunch2:'lunch', lunch3:'nigun',
